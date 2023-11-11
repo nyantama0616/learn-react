3e78e7c160be746ad643b8840b02397c35af6550
nyantama <r_nyantama0616@yahoo.co.jp>
2023-11-11 11:57:44 +0900

    MUIを勉強するための準備できた

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { LearnApexChart } from './apexcharts/LearnApexChart';
+import LearnMUI from './mui/LearnMUI';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<LearnApexChart />}></Route>
+          {/* <Route path="/" element={<LearnApexChart />}></Route> */}
+          <Route path="/" element={<LearnMUI />}></Route>
         </Routes>
       </Router>
     </div>
---
+++ src/mui/LearnMUI.tsx
@@ -0,0 +1,7 @@
+export default function LearnMUI() {
+    return (
+        <div>
+            <h1>Learn MUI</h1>
+        </div>
+    )
+}
