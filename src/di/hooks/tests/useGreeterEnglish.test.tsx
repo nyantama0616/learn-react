92e94a3b0168cbfb72903be39a906eb8f4307dcb
nyantama <r_nyantama0616@yahoo.co.jp>
2023-11-19 22:53:22 +0900

    [Test] useGreeterEnglishのテスト方法変えた
    タイマーを使ったhookのテストは、少々工夫が必要

--- src/di/hooks/tests/useGreeterEnglish.test.tsx
+++ src/di/hooks/tests/useGreeterEnglish.test.tsx
@@ -1,12 +1,30 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook, within, screen, waitFor, cleanup } from "@testing-library/react";
 import useGreeterEnglish from "../useGreeterEnglish";
 
 describe("test useGreeterEnglish", () => {
+    beforeEach(() => {
+        jest.useFakeTimers(); //リアルな時間を使わずに、jestのタイマーを使う
+    });
+
+    afterEach(() => {
+        cleanup(); //これを書かないと、greeter内のsetIntervalが動き続けるので、warningが出る
+        jest.runOnlyPendingTimers(); //保留中のタイマーを実行する(TODO: よく分かってない)
+        jest.useRealTimers(); //リアルな時間に戻す
+    });
+
+    function clock() {
+       jest.advanceTimersByTime(500);
+    }
+
     test("test useGreeterEnglish", async () => {
         const { result } = renderHook(() => useGreeterEnglish());
-
-        // await new Promise((r) => setTimeout(r, 1000));
-
+        
         expect(result.current.message).toBe("Hello");
+
+        act(clock);
+        expect(result.current.message).toBe("World");
+        
+        act(clock);
+        expect(result.current.message).toBe("☆☆☆");
     });
 });
