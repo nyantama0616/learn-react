fa13c4388a8d6bd3cccfc17a337ca14ee59b3176
nyantama <r_nyantama0616@yahoo.co.jp>
2023-12-01 23:24:50 +0900

    [MUI] LearnGrid0作成

---
+++ src/mui/LearnGrid0.tsx
@@ -0,0 +1,26 @@
+import { Grid } from '@mui/material';
+import MyBox from './MyBox';
+import MyBox2 from './MyBox2';
+import SignInForm from './SignInForm';
+import { AuthProvider } from './contexts/AuthContext';
+
+export default function LearnGrid0() {
+    return (
+        <AuthProvider>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <MyBox num={0} sx={{ aspectRatio: "4/1" }} />
+                </Grid>
+                <Grid item xs={2}>
+                    <MyBox num={1} sx={{ aspectRatio: "16/9" }} />
+                </Grid>
+                <Grid item xs={8}>
+                    <SignInForm sx={{ height: "500px" }} />
+                </Grid>
+                <Grid item xs={2}>
+                    <MyBox2 />
+                </Grid>
+            </Grid>
+        </AuthProvider>
+    )
+}
--- src/mui/LearnMUI.tsx
+++ src/mui/LearnMUI.tsx
@@ -1,29 +1,11 @@
 import { Grid, Box } from '@mui/material';
-import MyBox from './MyBox';
-import MyBox2 from './MyBox2';
-import SignInForm from './SignInForm';
-import useSignInManager from './hooks/useSignInManager';
-import { AuthProvider } from './contexts/AuthContext';
+import LearnGrid0 from './LearnGrid0';
+import LearnGrid1 from './LearnGrid1';
 
 export default function LearnMUI() {
     return (
-        <Box>
-            <AuthProvider>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <MyBox num={0} sx={{ aspectRatio: "4/1" }} />
-                    </Grid>
-                    <Grid item xs={2}>
-                        <MyBox num={1} sx={{aspectRatio: "16/9"}} />
-                    </Grid>
-                    <Grid item xs={8}>
-                        <SignInForm sx={{height: "500px"}} />
-                    </Grid>
-                    <Grid item xs={2}>
-                        <MyBox2 />
-                    </Grid>
-                </Grid>
-            </AuthProvider>
+        <Box sx={{ height: "800px", backgroundColor: "#ffeeee" }}>
+            <LearnGrid1 sx={{height: "100%"}} />
         </Box>
     )
 }
